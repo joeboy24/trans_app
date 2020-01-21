0f55eace9024371e1c1a93b350559af9d51de818
JoeBoy <joeboy24.jb@gmail.com>
2020-01-21 15:24:11 -0800

    My First Git Commit for TransApp

---
+++ components/Main.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+
+
+
+import {
+    Text
+} from 'react-native'
+
+class Main extends Component{
+    render(){
+        return(
+            <Text>sdfjkfjsdfjdflsdlfjsfjsjfdldjlj</Text>
+        )
+    }
+}
+
+
+export default Main;
